f53eace4a34419df9832ba0fdbe0fb0d15662b9d
hansborough <hansboroughlee@gmail.com>
2022-06-18 17:09:24 +0800

    update

--- src/x.js
+++ src/x.js
@@ -1,3 +1,4 @@
 import './x.css'
+import './y.css'
 console.log(css.toString())
 export default 'xxx'
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,21 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const base = require('./webpack.config.base.js')
+
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-    },
-    entry: './src/index.js',
-    output: {
-        filename: 'index.[contenthash].js',
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'hahahahhahaha',
-        template: 'src/assets/index.html'
-      })],
-      module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["css-loader"],
-          },
-        ],
+  ...base,
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
+    ],
+  },
 };
